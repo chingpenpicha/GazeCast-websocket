935aed3f531944f62a8aa16d4a99a5a4a6e9ce22
“Penpicha <char.chings@gmail.com>
2022-07-29 23:56:55 +0100

    fix text

--- pages/index.jsx
+++ pages/index.jsx
@@ -28,38 +28,37 @@ const Home = () => {
                 <p style={{ color: 'red' }}>{idErr}</p>
             </div>
             <p className="body">First of all, thank you for your interest in my Master Thesis study “GazeCast: Using a mobile device to eye-track gaze interaction on the public display”.</p>
-            <p className="body">The purpose of this experiment is to evaluate the effectiveness of an eye-tracker provided by a smartphone camera to that of a standard web camera. </p>
-            <p className="body">This study will take approximately 30 minutes.</p>
-            <p className="body">
-                The task you must complete is to follow the pink potato on the screen with your eyes, using a different sort of eye-tracker for each
-                section.
-            </p>
-            <div className="body">
+            <p className="body">The purpose of this experiment is to evaluate the effectiveness of an eye-tracker provided by a smartphone camera to that of a standard web camera. This study will take approximately 30 minutes. </p>
+
+            <div className="flex body">
+                <span>
+                    <p>There will be three primary sections to this study: one with a camera, one with a phone on a stand, and
+                        one with a phone in your hand.  The task you must complete is to follow the pink potato on the screen with your eyes, using a different sort of eye-tracker for each
+                        section.</p>
+                    <p>Before starting each task, you will have a training task to learn how to interact with the
+                        display. Then it will be followed by six more tasks. You will be asked to complete
+                        the questionnaire after completing each section.</p>
+                    <p>
+                        I'll record your time spent on the study as well as any screen-based gaze data. All participant
+                        data will be stored securely and in an anonymous form.
+                    </p>
+
+                </span>
                 <Image
                     src="/interface_eg.png"
                     alt="QR code to mobile eye tracker"
                     width={600}
-                    height={300}
+                    height={305}
                 />
             </div>
-            <p className="body">
-                There will be three primary sections to this study: one with a camera, one with a phone on a stand, and
-                one with a phone in your hand.
-            </p>
-            <p className="body">
-                I'll record your time spent on the study as well as any screen-based gaze data. All participant
-                data will be stored securely and in an anonymous form.
-            </p>
-            <p className="body">
-                Before starting each task, you will have a training task to learn how to interact with the
-                display. Then it will be followed by six more tasks. You will be asked to complete
-                the questionnaire after completing each section.
-            </p>
+
 
             <div className="body">
                 NOTE*
                 <ol className='body' style={{ listStyleType: "decimal" }}>
                     <li>If you have any questions, please ask.</li>
+                    <li>Try to follow the pink object every moment until it detects movement.</li>
+                    <li>Make sure to move your eyes, not your head. Try to keep your head centred all the time.</li>
                     <li>Stay relaxed and remember that the eye-tracker, not you, is being evaluated.</li>
                 </ol>
             </div>
