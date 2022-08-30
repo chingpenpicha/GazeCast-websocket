4e3333c8b0a2e9094dc7dc0d6f11b9c9e64ba4f6
“Penpicha <char.chings@gmail.com>
2022-08-30 17:39:07 +0100

    add process.env

--- firebase.js
+++ firebase.js
@@ -1,15 +1,3 @@
-
-
-// var firebaseApp = firebase.initializeApp({
-//   apiKey: "AIzaSyDnNj_L3Ogo0TXkGNhVLMH2nQ4ILIug6ig",
-//   authDomain: "eyevote-remote.firebaseapp.com",
-//   projectId: "eyevote-remote",
-//   storageBucket: "eyevote-remote.appspot.com",
-//   messagingSenderId: "842441638283",
-//   appId: "1:842441638283:web:e6470c3e849fbb3714ef8e",
-//   measurementId: "G-ETZZ4VK7MF"
-// });
-
 import { initializeApp } from "firebase/app";
 // import { getDatabase } from "firebase/database";
 import { getFirestore } from "firebase/firestore";
@@ -19,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   // ...
   // The value of `databaseURL` depends on the location of the database
-  apiKey: "AIzaSyBb6oytKcY-nJsyXopwiCN5aXAtqjJnVM0",
-  authDomain: "gazecast-ac4fa.firebaseapp.com",
-  projectId: "gazecast-ac4fa",
-  storageBucket: "gazecast-ac4fa.appspot.com",
-  messagingSenderId: "697799990654",
-  appId: "1:697799990654:web:f2d7352aadf6adbba9e72f",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BACKET,
+  messagingSenderId: process.env.MESSAGEING_SENDER_ID,
+  appId: process.env.APP_ID,
   // databaseURL: "https://DATABASE_NAME.firebaseio.com",
 };
 
--- next.config.js
+++ next.config.js
@@ -2,7 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    SEESO_KEY: 'dev_5lnmpgqq7pkxpmw9g9f4sxzogayxoopz4bhpe8sv',
+    SEESO_KEY: process.env.SEESO_KEY,
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+    PROJECT_ID:process.env.PROJECT_ID,
+    STORAGE_BACKET:process.env.STORAGE_BACKET,
+    MESSAGEING_SENDER_ID: process.env.MESSAGEING_SENDER_ID,
+    APP_ID: process.env.APP_ID,
   },
   async headers() {
     return [
